1e4a8cb8b6b656453b6628fad88a06e720f29070
thokjend <thokjend@online.no>
2024-09-11 11:13:24 +0200

    Update SkillList.jsx

--- src/components/SkillList.jsx
+++ src/components/SkillList.jsx
@@ -5,97 +5,68 @@ export function SkillList({ data, view, name }) {
 
   if (!data || !view) return null;
 
-  const iconList = [
-    "/Attack_icon.png",
-    "/Hitpoints_icon.png",
-    "/Mining_icon.png",
-    "/Strength_icon.png",
-    "/Agility_icon.png",
-    "/Smithing_icon.png",
-    "/Defence_icon.png",
-    "/Herblore_icon.png",
-    "/Fishing_icon.png",
-    "/Ranged_icon.png",
-    "/Thieving_icon.png",
-    "/Cooking_icon.png",
-    "/Prayer_icon.png",
-    "/Crafting_icon.png",
-    "/Firemaking_icon.png",
-    "/Magic_icon.png",
-    "/Fletching_icon.png",
-    "/Woodcutting_icon.png",
-    "/Runecraft_icon.png",
-    "/Slayer_icon.png",
-    "/Farming_icon.png",
-    "/Construction_icon.png",
-    "/Hunter_icon.png",
-    "",
+  const skillData = [
+    { skill: data.skills[1], icon: "/Attack_icon.png" },
+    { skill: data.skills[4], icon: "/Hitpoints_icon.png" },
+    { skill: data.skills[15], icon: "/Mining_icon.png" },
+    { skill: data.skills[3], icon: "/Strength_icon.png" },
+    { skill: data.skills[17], icon: "/Agility_icon.png" },
+    { skill: data.skills[14], icon: "/Smithing_icon.png" },
+    { skill: data.skills[2], icon: "/Defence_icon.png" },
+    { skill: data.skills[16], icon: "/Herblore_icon.png" },
+    { skill: data.skills[11], icon: "/Fishing_icon.png" },
+    { skill: data.skills[5], icon: "/Ranged_icon.png" },
+    { skill: data.skills[18], icon: "/Thieving_icon.png" },
+    { skill: data.skills[8], icon: "/Cooking_icon.png" },
+    { skill: data.skills[6], icon: "/Prayer_icon.png" },
+    { skill: data.skills[13], icon: "/Crafting_icon.png" },
+    { skill: data.skills[12], icon: "/Firemaking_icon.png" },
+    { skill: data.skills[7], icon: "/Magic_icon.png" },
+    { skill: data.skills[10], icon: "/Fletching_icon.png" },
+    { skill: data.skills[9], icon: "/Woodcutting_icon.png" },
+    { skill: data.skills[21], icon: "/Runecraft_icon.png" },
+    { skill: data.skills[19], icon: "/Slayer_icon.png" },
+    { skill: data.skills[20], icon: "/Farming_icon.png" },
+    { skill: data.skills[23], icon: "/Construction_icon.png" },
+    { skill: data.skills[22], icon: "/Hunter_icon.png" },
+    { skill: data.skills[0], icon: "" },
   ];
 
-  const fixedList = [
-    data.skills[1],
-    data.skills[4],
-    data.skills[15],
-    data.skills[3],
-    data.skills[17],
-    data.skills[14],
-    data.skills[2],
-    data.skills[16],
-    data.skills[11],
-    data.skills[5],
-    data.skills[18],
-    data.skills[8],
-    data.skills[6],
-    data.skills[13],
-    data.skills[12],
-    data.skills[7],
-    data.skills[10],
-    data.skills[9],
-    data.skills[21],
-    data.skills[19],
-    data.skills[20],
-    data.skills[23],
-    data.skills[22],
-    data.skills[0],
-  ];
+  return (
+    <>
+      <h2 className="header-text">Personal scores for {name}</h2>
+      <h2>Skills</h2>
 
-  if (data && view) {
-    return (
-      <>
-        <h2 className="header-text">Personal scores for {name}</h2>
-        <h2>Skills</h2>
-
-        <div className="grid-container">
-          {fixedList.map((skill, index) => (
-            <div
-              key={skill.id}
-              className="grid-item"
-              onMouseEnter={() => setHoveredSkill(skill)}
-              onMouseLeave={() => setHoveredSkill(null)}
-            >
-              {index === 23 ? (
-                <span>Total level: {skill.level}</span>
-              ) : (
-                <>
-                  <img src={iconList[index]} alt={`${skill.name} icon`} />
-                  <div className="skill-level">
-                    <span className="skill-part-top">{skill.level}</span>
-                    <span className="skill-separator"></span>
-                    <span className="skill-part-bottom">{skill.level}</span>
-                  </div>
-                </>
-              )}
-              {hoveredSkill === skill && (
-                <div className="tooltip">
-                  <strong>Rank:</strong>{" "}
-                  {skill.rank === -1 ? "Unranked" : skill.rank} <br />
-                  <strong>XP:</strong> {skill.xp}
+      <div className="grid-container">
+        {skillData.map(({ skill, icon }, index) => (
+          <div
+            key={skill.id}
+            className="grid-item"
+            onMouseEnter={() => setHoveredSkill(skill)}
+            onMouseLeave={() => setHoveredSkill(null)}
+          >
+            {index === 23 ? (
+              <span>Total level: {skill.level}</span>
+            ) : (
+              <>
+                <img src={icon} alt={`${skill.name} icon`} />
+                <div className="skill-level">
+                  <span className="skill-part-top">{skill.level}</span>
+                  <span className="skill-separator"></span>
+                  <span className="skill-part-bottom">{skill.level}</span>
                 </div>
-              )}
-            </div>
-          ))}
-        </div>
-      </>
-    );
-  }
+              </>
+            )}
+            {hoveredSkill === skill && (
+              <div className="tooltip">
+                <strong>Rank:</strong>{" "}
+                {skill.rank === -1 ? "Unranked" : skill.rank} <br />
+                <strong>XP:</strong> {skill.xp}
+              </div>
+            )}
+          </div>
+        ))}
+      </div>
+    </>
+  );
 }
